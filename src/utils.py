99314cbe48ac0920d49784e37b7df0f193702e67
clark3493 <37091734+clark3493@users.noreply.github.com>
2018-11-07 23:36:20 -0600

    Added collinear check function

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,32 @@
 import numpy as np
 
 
+def collinear(points):
+    """
+    Determine if the given points are all collinear.
+
+    Collinearity is determined by verifying that the cross product
+    of xi and X is zero for all xi, where:
+        - xi is the i'th coordinate minus the first coordinate
+        - X is the list coordinate minus the first coordinate
+
+    Point coordinates may be of arbitrary dimension but the number
+    of dimensions for each point must be the same.
+
+    Parameters
+    ----------
+    points: array_like (ndim=1)
+        List of coordinates to check.
+
+    Returns
+    -------
+    are_collinear: bool
+        Boolean indicating if the points are all collinear.
+    """
+    cross = [np.cross(point-points[0], points[-1]-points[0]) for point in points]
+    return np.all(c == 0. for c in cross)
+
+
 def distance(a, b):
     """
     Evaluate the Einstein summation on the given 1D vectors (Euclidian norm).
