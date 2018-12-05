1cf0c8e4aec25bdd27be1846a4430375a1d9dc90
clark3493 <37091734+clark3493@users.noreply.github.com>
2018-12-04 21:21:29 -0600

    Added 2D integration

--- src/element/_gauss.py
+++ src/element/_gauss.py
@@ -6,9 +6,16 @@ from scipy.optimize import fsolve
 
 
 @lru_cache(maxsize=16)
-def gauss_points(ngp, ndims=1):
+def gauss_points(ngp, integration='rectilinear'):
     """
     Compute the Gauss point values and weights for an arbitrary number of Gauss points.
+
+    Parameters
+    ----------
+    ngp: int
+        Number of gauss points to compute.
+    integration: str, {'rectilinear', 'triangular'}
+        Type of Gaussian integration to be performed with the gauss points.
 
     Returns
     -------
@@ -16,38 +23,51 @@ def gauss_points(ngp, ndims=1):
         Gauss point values.
     w: list(float)
         Gauss point weights.
+
+    References
+    ----------
+    .. [1] Fish, Jacob and Belytschko, Ted. "A First Course in Finite Elements."
+           John Wiley & Sons, 2007. Chapters 4.6, 7.8.
     """
-    if ndims == 1:
+    if integration == 'rectilinear':
         return _gauss_points_1d(ngp)
     else:
-        raise NotImplementedError("Gauss quadrature for greater than 1 dimension has not been implemented.")
+        raise NotImplementedError("Gauss quadrature for triangular domains has not been implemented.")
 
 
-def weighted_integration(func, values, weights):
+def weighted_integration(func, values, weights, ndims=1):
     """
     Perform weighted numerical integration on a function with given values and weights.
 
-    Input func can either be a function or a vector of functions.
+    Input func can either be a function or an array of functions.
+
+    For integration over a number of dimensions greater than 1, the integration
+    if performed for the same values and weights in each direction.
 
     Parameters
     ----------
-    func: function or list(function)
-        The function or list of functions to perform
+    func: function or array_like(function)
+        The function or array of functions to perform
     values: list(float)
         The values at which to evaluate the function.
     weights: list(float)
-        Weights correspoding to each value.
+        Weights corresponding to each value.
+    ndims: int
+        Number of dimensions over which to perform integration.
 
     Returns
     -------
     I: float or list(float)
         Integrated function or vector of integrated functions.
     """
-    try:
-        v = [weighted_integration(f, values, weights) for f in func]
-        return np.array(v).reshape(func.shape)
-    except TypeError:
-        return sum([func(v)*w for v, w in zip(values, weights)])
+    vfunc = np.array(func).flatten()
+    if ndims == 1:
+        v = np.array([_weighted_integration_1d(f, values, weights) for f in vfunc])
+    elif ndims == 2:
+        v = np.array([_weighted_integration_2d(f, values, weights) for f in vfunc])
+    else:
+        raise NotImplementedError("Weighted (Gaussian) integration for more than two dimensions has not been implemented")
+    return v.reshape(func.shape)
 
 
 @lru_cache(maxsize=16)
@@ -127,3 +147,49 @@ def _P(n):
     Function inputs are cached so that calculations are not repeated on each call.
     """
     return [integrate.quad(lambda x: x**i, -1., 1.)[0] for i in range(n)]
+
+
+def _weighted_integration_1d(func, values, weights):
+    """
+    Perform weighted numerical integration of a function in 1 dimension.
+
+    Parameters
+    ----------
+    func: function
+        The function to integrate.
+    values: list(float)
+        The values at which to evaluate the function.
+    weights: list(float)
+        The weights corresponding to each value.
+
+    Returns
+    -------
+    sum: float
+        Result of the numerical integration.
+    """
+    return sum([func(v)*w for v, w in zip(values, weights)])
+
+
+def _weighted_integration_2d(func, values, weights):
+    """
+    Perform weighted numerical integration of a function in 2 dimensions.
+
+    The same values and weights are used for integration in each direction.
+
+    func: function
+        The function to integrate.
+    values: list(float)
+        The values at which to evaluate the function.
+    weights: list(float)
+        The weights corresponding to each value.
+
+    Returns
+    -------
+    sum: float
+        Result of the numerical integration.
+    """
+    sum = 0.
+    for i in range(len(values)):
+        for j in range(len(values)):
+            sum += func(values[i], values[j]) * weights[i] * weights[j]
+    return sum
